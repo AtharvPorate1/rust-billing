0cf523396cc878369fefa5159da90967e2021435
[AtharvPorate1] <www.atharvporate254@gmail.com>
2024-06-02 23:20:36 +0530

    initialized database with place holder fields and data

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use std::env::args;
+use rusqlite::{params, Connection};
 
 fn main() {
+
+    let conn = Connection::open("file.db").expect("Connection failed");
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS billing(
+            id INTEGER PRIMARY KEY,
+            data TEXT NOT NULL)"
+        , ()).expect("Failed to create a database");
+
+
     
     let args: Vec<String> = args().collect();
 
